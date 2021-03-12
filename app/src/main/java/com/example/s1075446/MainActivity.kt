3976aa3be89bfe9d01ddf0c92156ce16af29b7c6
s1075446 <s1075446@gm.pu.edu.tw>
2021-03-12 14:39:49 +0800

    加入master分支程式碼

--- app/src/main/java/com/example/s1075446/MainActivity.kt
+++ app/src/main/java/com/example/s1075446/MainActivity.kt
@@ -8,7 +8,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Hello tcyang"
+        TestMaster()
+
+
     }
+    fun TestMaster(){
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Master分支"
+    }
+
+
 }
-var txv:TextView = findViewById<TextView>(R.id.txv)
-txv.text = "Hello tcyang"
