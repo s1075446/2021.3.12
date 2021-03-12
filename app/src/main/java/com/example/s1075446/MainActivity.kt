cce5a4d2394c2343f4dbfc01233717cfa6234a00
s1075446 <s1075446@gm.pu.edu.tw>
2021-03-12 14:37:46 +0800

    加入dev分支程式碼

--- app/src/main/java/com/example/s1075446/MainActivity.kt
+++ app/src/main/java/com/example/s1075446/MainActivity.kt
@@ -8,7 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Hello tcyang"
+        TestDEV()
+
+
+    }
+    fun TestDEV(){
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Dev分支"
     }
 }
-var txv:TextView = findViewById<TextView>(R.id.txv)
-txv.text = "Hello tcyang"
