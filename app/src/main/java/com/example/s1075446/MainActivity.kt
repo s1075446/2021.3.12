3614430d677cc8e12084724ff3e7b35b276085aa
s1075446 <s1075446@gm.pu.edu.tw>
2021-03-12 14:12:19 +0800

    第二次commit (修改文字元件)

--- app/src/main/java/com/example/s1075446/MainActivity.kt
+++ app/src/main/java/com/example/s1075446/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.s1075446
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +10,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+var txv:TextView = findViewById<TextView>(R.id.txv)
+txv.text = "Hello tcyang"
